d254a6c9a1e634869c9758c95bd0858148f399a6
Yannick Heinrich <yannick.heinrich@gmail.com>
2014-12-03 16:08:17 +0100

    First commit

---
+++ lambert-cs/Point.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+
+namespace lambertcs
+{	
+	public enum Unit{Degree, Grad, Radian, Meter};
+
+	public class Point
+	{
+
+		private const double degreeToradian = Math.PI/180.0;
+		private const double radianTodegree = 180.0/Math.PI;
+
+		private const double gradTodegree = 180.0/200.0;
+		private const double degreeTograd = 200.0/180.0;
+
+		private const double gradToradian = Math.PI/200.0;
+		private const double radianTograd = 200.0/Math.PI;
+
+
+		public Point (double x, double y, double z)
+		{
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+
+		public double x { get; private set;}
+		public double y { get; private set;}
+		public double z { get; private set;}
+
+		public Unit unit { get{ return unit; } 
+			set { 
+
+				switch (value) {
+				case Unit.Degree:
+					ToDegree ();
+					break;
+				case Unit.Grad:
+					ToGrad ();
+					break;
+				case Unit.Radian:
+					ToRadian ();
+					break;
+				}
+			} }
+
+		private void Scale(double scale){
+			this.x *= scale;
+			this.y *= scale;
+			this.z *= scale;
+		}
+
+		private void ToDegree(){
+			switch (unit) {
+			case Unit.Radian:
+				Scale (radianTodegree);
+				break;
+			case Unit.Grad:
+				Scale (gradTodegree);
+				break;
+			default:
+				break;
+			}
+		}
+
+		private void ToGrad(){
+			switch(unit){
+			case Unit.Degree:
+				Scale (gradTodegree);
+				break;
+			case Unit.Radian:
+				Scale (radianTograd);
+				break;
+			default:
+				break;
+			}
+		}
+
+		private void ToRadian(){
+			switch (unit) {
+			case Unit.Degree:
+				Scale (degreeTograd);
+				break;
+			case Unit.Grad:
+				Scale (gradToradian);
+				break;
+			default:
+				break;
+			}
+		}
+	}
+}
+
